66994d1e746c8178b581f4579684164d92b45ae0
ValentinCamus <ad.valentin.camus@gmail.com>
2019-09-13 21:37:43 +0200

    Add Material class from v2

---
+++ Source/Alpha/Engine/Material/Material.cpp
@@ -0,0 +1,82 @@
+#include "Material.h"
+
+namespace Alpha
+{
+    void Material::Bind(Pointer<Shader> shader)
+    {
+        Pointer<Texture2D> textureKd = GetTexture(TextureType::TX_Diffuse);
+        if (textureKd)
+        {
+            textureKd->Bind(0);
+            shader->SetUniform(m_name + ".tex.kd", textureKd->GetSlot());
+            shader->SetUniform(m_name + ".tex.hasKd", true);
+        }
+        else
+        {
+            shader->SetUniform(m_name + ".tex.hasKd", false);
+            shader->SetUniform(m_name + ".kd", m_kd);
+        }
+
+        Pointer<Texture2D> textureKs = GetTexture(TextureType::TX_Specular);
+        if (textureKs)
+        {
+            textureKs->Bind(1);
+            shader->SetUniform(m_name + ".tex.ks", textureKs->GetSlot());
+            shader->SetUniform(m_name + ".tex.hasKs", true);
+        }
+        else
+        {
+            shader->SetUniform(m_name + ".ks", m_ks);
+            shader->SetUniform(m_name + ".tex.hasKs", false);
+        }
+
+        Pointer<Texture2D> textureRough = GetTexture(TextureType::TX_Roughness);
+        if (textureRough)
+        {
+            textureRough->Bind(2);
+            shader->SetUniform(m_name + ".tex.roughness", textureRough->GetSlot());
+            shader->SetUniform(m_name + ".tex.hasRoughness", true);
+        }
+        else
+        {
+            shader->SetUniform(m_name + ".roughness", m_roughness);
+            shader->SetUniform(m_name + ".tex.hasRoughness", false);
+        }
+
+        Pointer<Texture2D> textureNormal = GetTexture(TextureType::TX_Normal);
+        if (textureNormal)
+        {
+            textureNormal->Bind(3);
+            shader->SetUniform(m_name + ".tex.normal", textureNormal->GetSlot());
+            shader->SetUniform(m_name + ".tex.hasNormal", true);
+        }
+        else
+        {
+            shader->SetUniform(m_name + ".normal", m_normal);
+            shader->SetUniform(m_name + ".tex.hasNormal", false);
+        }
+
+        Pointer<Texture2D> textureTransparency = GetTexture(TextureType::TX_Transparency);
+        if (textureTransparency)
+        {
+            textureTransparency->Bind(4);
+            shader->SetUniform(m_name + ".tex.transparency", textureTransparency->GetSlot());
+            shader->SetUniform(m_name + ".tex.hasTransparency", true);
+        }
+        else
+        {
+            shader->SetUniform(m_name + ".transparency", m_transparency);
+            shader->SetUniform(m_name + ".tex.hasTransparency", false);
+        }
+    }
+
+    void Material::Unbind()
+    {
+        for (auto& node : m_textures)
+        {
+            Pointer<Texture2D>& texture = node.second;
+            texture->Unbind();
+        }
+    }
+}
+
---
+++ Source/Alpha/Engine/Material/Material.h
@@ -0,0 +1,128 @@
+#pragma once
+
+#include <Alpha/Core/CoreMinimal.h>
+
+#include <Alpha/Engine/Renderer/Shader.h>
+#include <Alpha/Engine/Renderer/Texture.h>
+
+namespace Alpha
+{
+    class Material
+    {
+    public:
+        /// The type of the texture.
+        enum TextureType
+        {
+            TX_Diffuse,
+            TX_Specular,
+            TX_Normal,
+            TX_Roughness,
+            TX_Transparency
+        };
+
+    public:
+        /// Constructor.
+        /// @name: The uniform name.
+        explicit Material(const std::string &name = "material") : m_name(name)
+        {
+            Logger::Info("Creating Material: {0}", name);
+        }
+
+        /// Destructor.
+        virtual ~Material() { m_textures.clear(); }
+
+        /// Bind this material.
+        virtual void Bind(Pointer<Shader> shader);
+
+        /// Unbind this material.
+        virtual void Unbind();
+
+    public:
+        /// Add a new texture.
+        inline void AddTexture(const TextureType& type, const Pointer<Texture2D>& texture)
+        {
+            m_textures.insert({type, texture});
+        }
+
+        /// Remove a texture.
+        /// @return: False is texture not found.
+        inline bool RemoveTexture(const TextureType& type)
+        {
+            auto it = m_textures.find(type);
+            bool isFound = (it != m_textures.end());
+
+            if (isFound) m_textures.erase(type);
+
+            return isFound;
+        }
+
+        /// Clear all the textures.
+        inline void ClearTextures() { m_textures.clear(); }
+
+        /// Get the texture that match the type in argument.
+        inline Pointer<Texture2D> GetTexture(const TextureType& type)
+        {
+            auto it = m_textures.find(type);
+            return (it != m_textures.end()) ? it->second : nullptr;
+        }
+
+    public:
+        /// @getter: The Kd color of the material.
+        inline const Vector4& GetKd() const { return m_kd; }
+
+        /// @setter: The Kd color of the material.
+        inline const Vector4& SetKd(const Vector4& kd) { return m_kd = kd; }
+
+        /// @getter: The Ks color of the material.
+        inline const Vector4& GetKs() const { return m_ks; }
+
+        /// @setter: The Ks color of the material.
+        inline const Vector4& SetKs(const Vector4& ks) { return m_ks = ks; }
+
+        /// @getter: The roughness of the material.
+        inline const Vector3& GetNormal() const { return m_normal; }
+
+        /// @setter: The roughness of the material.
+        inline const Vector3& SetNormal(const Vector3& normal) { return m_normal = normal; }
+
+        /// @getter: The roughness of the material.
+        inline float GetRoughness() const { return m_roughness; }
+
+        /// @setter: The roughness of the material.
+        inline float SetRoughness(float roughness) { return m_roughness = roughness; }
+
+        /// @getter: The transparency of the material.
+        inline float GetTransparency() const { return m_transparency; }
+
+        /// @setter: The transparency of the material.
+        inline float SetTransparency(float transparency) { return m_transparency = transparency; }
+
+    private:
+        /// The name of the uniform.
+        std::string m_name;
+
+        /// The Kd (diffuse) color.
+        /// @note: m_kd color has a lower priority than TX_Diffuse.
+        Vector4 m_kd = Vector4(1);
+
+        /// The Ks (specular) color.
+        /// @note: m_ks color has a lower priority than TX_Specular.
+        Vector4 m_ks = Vector4(1);
+
+        /// The normal color.
+        /// @note: m_normal color has a lower priority than TX_Normal.
+        Vector3 m_normal = Vector3(1);
+
+        /// The roughness.
+        /// @note: m_roughness has a lower priority than TX_Roughness.
+        float m_roughness = 0.0f;
+
+        /// The transparency of the material.
+        /// Where 1 means opaque and 0 means totally transparent.
+        /// @note: m_transparency has a lower priority than TX_Transparency.
+        float m_transparency = 1.0f;
+
+        /// Textures Map.
+        std::map<TextureType, Pointer<Texture2D>> m_textures;
+    };
+}
